869e9ac6c517fdec448c52ab9a77e023763cfd5b
BlueRam <dr.odiram@gmail.com>
2020-10-04 02:07:49 +0300

    'final_parse'

--- Course2/parse.py
+++ Course2/parse.py
@@ -1,38 +1,16 @@
-# def parse(query: str) -> dict:
-#     pars_dict = dict(parse.parse_qsl(parse.urlsplit(query).query))
-#     return pars_dict
+def parse(query: str) -> dict:
+    query_temp_list = ((query.partition('?'))[-1]).split('&')
+    query_temp_dict = {}
+    for i in query_temp_list:
+        if '=' in i:
+            query_temp_dict[(i.partition('=')[0])] = (i.partition('='))[-1]
+    return query_temp_dict
 
 
-# if __name__ == '__main__':
-#     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-#     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-#     assert parse('http://example.com/') == {}
-#     assert parse('http://example.com/?') == {}
-#     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
-url = 'http://example.com/'
-first_slice = (url.partition('?'))
-query_temp_list = []
-for i in first_slice:
-    if '=' in i:
-        query_temp_list.append(i)
-        print(query_temp_list)
-
-# print(first_slice[-1])
-# if first_slice[-1] == '':
-#     print('empty')
-# print((str(first_slice[-1])).partition('?'))
-# print(url[-1])
-# if (url[-1]) == '?' or (url[-1]) == '&':
-#     query_string = str((url.partition('?'))[-1])
-# else:
-#     query_string = str((url.partition('?'))[-2])
-# print(query_string)
-# query_list = query_string.split('&')
-# print(query_list)
-# final_dict = {}
-# for i in query_list:
-#     temp_list = (str(i)).split('=')
-#     print(temp_list)
-#     #final_dict.setdefault(temp_list[0], temp_list[1])
-# print(final_dict)
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+print(parse('https://example.com/path/to/page?name=ferret&color=purple&'))
