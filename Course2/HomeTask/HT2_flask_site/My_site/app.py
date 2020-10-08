09085011213ff63cd0d7ede7fb80774cbe02f607
BlueRam <dr.odiram@gmail.com>
2020-10-08 14:49:20 +0300

    HT2

--- Course2/HomeTask/HT2_flask_site/My_site/app.py
+++ Course2/HomeTask/HT2_flask_site/My_site/app.py
@@ -1,40 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from faker import Faker
 import requests
 
 
-# app = Flask(__name__, template_folder='requirements')
 app = Flask(__name__)
 
 
-@app.route('/index/')
+@app.route('/')
 def home():
-    somee_var = 12
-    user = {
-        'name': 'BlueRam',
-        'age': 29
-    }
-    # with open('requirements/requirements.txt') as tmps:
-    # with open('templates/index.html') as tmps:
-    # return tmps.read()
-    context = {
-        'some_var': somee_var,
-        'user': user
-    }
-    return render_template(
-        'requirements/requirements.txt', **context
-    )
+    return render_template('index.html')
 
 
-@app.route('/generate_users/')
+@app.route('/requirements/')
+def requirements():
+    with open('templates/requirements/requirements.txt') as required:
+        requirements_list = required.read().splitlines()
+        return render_template('/requirements/requirements.html', requirements_list=requirements_list)
+
+
+@app.route('/users/', methods=['POST', 'GET'])
 def generate_users():
     fake = Faker()
-    fake_name = fake.name()
+    users = {}
+    number_of_users = 0
+    if request.method == 'POST' and request.form['usr_num'].isdigit():
+        number_of_users = int(request.form['usr_num'])
+    for random_user in range(number_of_users):
+        temp_name = fake.name()
+        users[temp_name] = str(temp_name.replace(' ', ''))+'@mail.com'
+    return render_template('/generate_users/generate_users.html', users=users, number_of_users=number_of_users)
+
+
+@app.route('/astros/')
+def astros():
+    astro_request = requests.get('http://api.open-notify.org/astros.json')
+    number_of_astros = astro_request.json()["number"]
+    astros_in_space = astro_request.json()["people"]
     context = {
-        'name': fake_name
+        'number_of_astros': number_of_astros,
+        'astros_in_space': astros_in_space
     }
-    # return render_template('generate_users/generate_users.html', **context)
-    return f'{fake.name()} : {fake.name()}@mail.com' in range(100)
+    return render_template('/astros/astros.html', **context)
 
 
 if __name__ == '__main__':
